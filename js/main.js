69933a343edcb5146da6efb1fb2f71e66c13adac
thisisdavidlau <davidlaului@gmail.com>
2016-07-25 21:40:00 -0700

    (nick)
    in class work

---
+++ js/main.js
@@ -0,0 +1,179 @@
+// console.log("hello world")
+
+
+// var car = prompt("what car do you have?")
+// alert("you have a " + car)
+
+
+// var room = "room 3"
+// var num = 101
+// var className = "architecture"
+// var bool = false
+// var arr = [num, name, bool]
+
+
+// var typeClass = room + ", " + className + " " + num + "."
+
+
+// var user = prompt("What is your name?")
+// if (user == "david") {
+// 	document.write("Your class is in " + typeClass)
+// }
+
+
+
+
+// for (var i = 1; i <= 20; i++) {
+// 	console.log(i)
+// }
+
+
+
+// function multiplyNum(a, b) {
+// 	return console.log(5 * 10)
+// }
+
+// multiplyNum()
+
+
+
+
+// var yourName = prompt("What is your name?")
+// alert("Welcome to this website " + yourName + ".")
+
+
+
+
+// var yourName = prompt("What is your name?").toLowerCase()
+// var name1 =  "alice"
+// var name2 = "bob"
+
+// function capitalizeFirstLetter(yourName) {
+//     return yourName.charAt(0).toUpperCase();
+// }
+// yourName.capitalizeFirstLetter()
+
+// if (yourName == name1 || yourName == name2) {
+
+// 	alert("Welcome to this website " + yourName+ ".")
+
+// }else {
+// 	alert("We don't want you here!!")
+
+// }
+
+// document.getElementById('message').innerHTML = "<h1>hello</h1>"
+
+
+// function myFucntion() {
+// 	var x = document.getElementById('myList').textContent
+
+// 	document.getElementById('demo').innerHTML = x
+// }
+
+// myFucntion()
+
+
+// function getInfo() {
+// 	var userInput = document.getElementById('username').value
+// 	var password = document.getElementById('password').value
+
+// 	document.getElementById('yourInfo').textContent = "your username is " + userInput + " and your password is " + password
+
+// }
+
+
+
+
+// var cars = ["honda", "lexus", "merc", "toyota"]
+
+// console.log("the first car is a " + cars[0] + " and the second car is a " + cars[1])
+
+// BIGGEST NUMBER SEARCH//////////////////////////////////
+// var nums = [10, 20, 30, 60, 50]
+// var big = 0
+
+// function findBiggest(){
+// for (var i = big; i <= nums.length; i++) {
+// 	if (big < nums[i]) {
+// 		big = nums[i];
+// 		}
+// 	}
+// }
+// findBiggest();
+// console.log(big)
+
+
+// var carObject = {
+// 	make: "fiat",
+// 	year: 2010,
+// 	purchased: false
+// }
+
+// console.log(carObject.year)
+
+
+
+
+
+
+
+
+
+
+
+
+// var account = [
+// 	{
+// 		name: "david",
+// 		password: 123
+// 	},
+// 	{
+// 		name: "john",
+// 		password: 456
+// 	},
+// 	{
+// 		name: "jane",
+// 		password: 789
+// 	}
+// ]
+
+
+// function getInfo() {
+// 	var username = document.getElementById('username').value
+// 	var password = document.getElementById('password').value
+
+
+// for (i = 0; i < account.length; i++) {
+// 	if (username == account[i].name && password == account[i].password) {
+// 		alert(username + " is logged in!")
+// 		console.log(username + " is logged in!")
+// 		return
+// 		}
+// 	}
+
+// 	alert("incorrect username or password")
+// }
+
+
+
+
+var name = prompt("What is your name?")
+alert(name + "?? NOBODY CARES!! GET THE FUCK OUTTA HERE!!!!!!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
